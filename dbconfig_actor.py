8d053d50c6a2e8d90b89748cbe41729c21bf86c7
Filipe Carvalho <filipeferc88@gmail.com>
2025-05-16 02:32:00 +0100

    server dbconfig_actor

---
+++ dbconfig_actor.py
@@ -0,0 +1,6 @@
+mysql = {
+    'host':"filipekojak88.mysql.pythonanywhere-services.com",
+    'user':"filipekojak88",
+    'password':"280788WSAAproject",
+    'database':"filipekojak88$actors_db"
+}
