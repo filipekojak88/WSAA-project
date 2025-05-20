c365564ae852a2c304c3c7fa36915c95b6599c46
Filipe Carvalho <filipeferc88@gmail.com>
2025-05-20 12:08:43 +0100

    Improved comments and tidied up code in dbconfig.py and tmdb_service.py

--- dbconfig.py
+++ dbconfig.py
@@ -1,6 +1,10 @@
-# This is a database configuration file for a MySQL database and TMDB API.
-# Author: Filipe Carvalho
+"""
+Title: Database Configuration
+Description: This file contains the configuration settings for connecting to a MySQL database and TMDB API.
+#Author: Filipe Carvalho
+"""
 
+# MySQL database configuration
 mysql = {
     'host':"filipekojak88.mysql.pythonanywhere-services.com",
     'user':"filipekojak88",
@@ -8,6 +12,7 @@ mysql = {
     'database':"filipekojak88$actors_db"
 }
 
+# TMDB API configuration
 tmdb = {
     'api_key': "f2e9f772126f1b0d2dd7c2bb472a9bf2",
     'bearer_token': "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmU5Zjc3MjEyNmYxYjBkMmRkN2MyYmI0NzJhOWJmMiIsIm5iZiI6MTc0NzYwMzY4MS44MjIsInN1YiI6IjY4MmE1MGUxMTI0NTg1NTkxN2RiMmI2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FmxYIrubjlPiothTRWp4xHqSy0yVii_EO-2Q_00xySE",
--- tmdb_service.py
+++ tmdb_service.py
@@ -1,37 +1,57 @@
+"""
+Title: TMDB Service
+Description: This module provides a service to interact with the TMDB API for searching and retrieving actor details.
+Author: Filipe Carvalho
+"""
+
+# Import necessary libraries
 import requests
 import dbconfig as cfg
 
 class TMDBService:
+    """Service to interact with TMDB API"""
+    
+    # API Configuration
+    headers = {
+        "Authorization": f"Bearer {cfg.tmdb['bearer_token']}",
+        "accept": "application/json"
+    }
+    default_params= {"language": "en-US"}
+
     @staticmethod
     def search_actors(query, page=1):
+        # Search for actors by name
         url = f"{cfg.tmdb['base_url']}/search/person"
-        headers = {
-            "Authorization": f"Bearer {cfg.tmdb['bearer_token']}",
-            "accept": "application/json"
-        }
+        
         params = {
+            **TMDBService.default_params,
             "query": query,
             "include_adult": False,
             "language": "en-US",
             "page": page
       }
     
-        response = requests.get(url, headers=headers, params=params)
+        response = requests.get(
+            url, 
+            headers=TMDBService.headers, 
+            params=params
+        )
         response.raise_for_status()
         return response.json()
 
     @staticmethod
     def get_actor_details(actor_id):
+        # Get detailed information about an actor
         url = f"{cfg.tmdb['base_url']}/person/{actor_id}"
-        headers = {
-            "Authorization": f"Bearer {cfg.tmdb['bearer_token']}",
-            "accept": "application/json"
-        }
         params = {
-            "language": "en-US",
+            **TMDBService.default_params,
             "append_to_response": "combined_credits"
         }
         
-        response = requests.get(url, headers=headers, params=params)
+        response = requests.get(
+            url, 
+            headers=TMDBService.headers,
+            params=params
+        )
         response.raise_for_status()
         return response.json()
